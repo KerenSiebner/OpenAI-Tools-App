9aaa9182fbf31a2ab2d123328dc75838a290978c
Keren Siebner <kerens969@gmail.com>
2023-03-16 15:44:19 +0200

    Added fix grammar feature using openAI

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,30 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './App.css';
+import { Grammar } from './cmps/grammar';
 import { Sentiment } from './cmps/sentiment';
 import RootLayout from './layout/root-layout';
 
 
-const OPENAI_API_KEY = process.env.REACT_APP_api_key
+const OPENAI_API_KEY: String | undefined = process.env.REACT_APP_api_key
 console.log('OPENAI_API_KEY', OPENAI_API_KEY)
-interface Data {
-  choices: [],
-  created: number,
-  id: string,
-  model: string,
-  object: string,
-  usage: any
-}
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout/>}>
-      <Route index element={<Sentiment/>}/>
-    </Route>
-  )
-)
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<RootLayout/>}>
+//       <Route index element={<Sentiment/>}/>
+//     </Route>
+//   )
+// )
 function App() {
-  const [tweet, setTweet] = useState("")
-  const [sentiment, setSentiment] = useState("")
-
-  async function callOpenAIAPI() {
-
-    const APIBody = {
-      "model": "text-davinci-003",
-      "prompt": "What is the sentiment of this tweet?" + tweet,
-      "temperature": 0,
-      "max_tokens": 60,
-      "top_p": 1.0,
-      "frequency_penalty": 0.0,
-      "presence_penalty": 0.0
-    }
-
-    try {
-      let data: Data|Response = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${OPENAI_API_KEY}`
-        },
-        body: JSON.stringify(APIBody)
-      })
-      return data = await data.json()
-    } catch (err) {
-      console.log("Failed to fetch data")
-    }
-  }
-  
-  async function getSentiment(){
-    try{
-      const data = await callOpenAIAPI()
-      setSentiment(data.choices[0].text.trim())
-    } catch (err){
-      console.log('Failed to retrieve sentiment')
-    }
-    
-  }
-
-  return (
-            <div className="App">
-        <h1>Is my content positive?</h1>
-        <textarea
-          onChange={(e) => setTweet(e.target.value)}
-        ></textarea>
-        <button onClick={getSentiment}>What is my content sentiment?</button>
-        <h3>OpenAI Response:</h3>
-        <div className='ai-response'>
-          {sentiment !== "" ?
-            <h3 > <span className={sentiment}>
-              {sentiment.replace('tweet', 'content')}
-            </span>  </h3>
-            :
-            null
-          }
-        </div>
-    </div>
-  )
+  return <div>
+    <header>
+      <button className='nav-btn'>Content Sentiment Detector</button>
+      <button className='nav-btn'>Grammer Correction</button>
+    </header>
+    {/* <Sentiment OPENAI_API_KEY={OPENAI_API_KEY} /> */}
+    <Grammar OPENAI_API_KEY={OPENAI_API_KEY}/>
+  </div>
 }
 
 export default App;
--- src/cmps/grammar.tsx
+++ src/cmps/grammar.tsx
@@ -8,15 +8,15 @@ interface Data {
     usage: any
   }
 
-export function Sentiment({OPENAI_API_KEY}:any){
+export function Grammar({OPENAI_API_KEY}:any){
     const [content, setContent] = useState("")
-    const [sentiment, setSentiment] = useState("")
+    const [correctedContent, setCorrectedContent] = useState("")
   
     async function callOpenAIAPI() {
   
       const APIBody = {
         "model": "text-davinci-003",
-        "prompt": "What is the sentiment of this content?" + content,
+        "prompt": `Correct this to standard English: ${content}`,
         "temperature": 0,
         "max_tokens": 60,
         "top_p": 1.0,
@@ -39,28 +39,29 @@ export function Sentiment({OPENAI_API_KEY}:any){
       }
     }
     
-    async function getSentiment(){
+    async function getCorrectedContent(){
       try{
         const data = await callOpenAIAPI()
-        setSentiment(data.choices[0].text.trim())
+        console.log('data', data)
+        setCorrectedContent(data.choices[0].text.trim())
       } catch (err){
-        console.log('Failed to retrieve sentiment')
+        console.log('Failed to retrieve correctedContent')
       }
       
     }
   
     return (
               <div className="App">
-          <h1>Is my content positive?</h1>
+          <h1>Fix my grammar!</h1>
           <textarea
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
-          <button onClick={getSentiment}>What is my content sentiment?</button>
+          <button onClick={getCorrectedContent}>Help me out!</button>
           <h3>OpenAI Response:</h3>
           <div className='ai-response'>
-            {sentiment !== "" ?
-              <h3 > <span className={sentiment}>
-                {sentiment.replace('tweet', 'content')}
+            {correctedContent !== "" ?
+              <h3 > <span className={correctedContent}>
+                {correctedContent.replace('content', 'content')}
               </span>  </h3>
               :
               null
